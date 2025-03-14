6d452159342570827f06263af6eb8d3abf2eaf94
Tofik Haziyev <toxageek@gmail.com>
2025-03-14 23:19:36 +0200

    fix: add annotation to Facade

--- src/Facades/Binotel.php
+++ src/Facades/Binotel.php
@@ -4,6 +4,9 @@ namespace Toxageek\BinotelLaravel\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @see \Toxageek\BinotelLaravel\BinotelClient
+ */
 class Binotel extends Facade
 {
     protected static function getFacadeAccessor(): string
